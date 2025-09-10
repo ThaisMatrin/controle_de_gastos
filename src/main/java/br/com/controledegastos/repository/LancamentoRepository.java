04dcbb84ab2f8fc09f7cfc8056cc7d7c17df2c50
ThaisMatrin <thatamatrin@gmail.com>
2025-09-09 21:27:06 -0300

    first commit

--- src/main/java/br/com/controledegastos/repository/LancamentoRepository.java
+++ src/main/java/br/com/controledegastos/repository/LancamentoRepository.java
@@ -1,4 +1,10 @@
-package br.com.controledegastos.Repository;
+// src/main/java/br/com/controledegastos/repository/LancamentoRepository.java
+package br.com.controledegastos.repository;
 
-public interface LancamentoRepository {
+import br.com.controledegastos.model.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 }
